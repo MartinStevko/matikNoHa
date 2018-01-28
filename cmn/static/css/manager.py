4ce8ed0beae465903f82f0085eb29e3f02812072
MartinStevko <mstevko10@gmail.com>
2018-01-28 01:13:54 +0100

    Create manager.py

---
+++ cmn/static/css/manager.py
@@ -0,0 +1,31 @@
+#gen {
+  padding-left: 15px;
+  padding-right: 15px;
+  padding-top: 15px;
+}
+
+#mess {
+  padding-bottom: 15px;
+}
+
+#bubble {
+  display: block;
+  padding: 10px;
+	position: relative;
+	background: #eaebff;
+	border-radius: .6em;
+  font-size: 135%;
+  color: black;
+  text-decoration: none;
+}
+
+#cas {
+  text-align: right;
+  color: #afafaf;
+}
+
+#author {
+  padding-left: 7px;
+  font-size: 120%;
+  color: #afafaf;
+}
